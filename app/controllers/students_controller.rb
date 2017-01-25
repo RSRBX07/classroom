86b29c86cb7d68892da993627b4603458728227c
Dodo <donovan.persent@gmail.com>
2017-01-25 17:07:44 +0100

    End of the day commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,8 @@ class StudentsController < ApplicationController
  
   def index 
      @students= Student.all
-     
+     @courses= Course.all
+
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'students/index'
-
   get 'students/show'
+  get 'courses/index'
+  get 'courses/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
