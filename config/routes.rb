ac07969ccbb1ce882a57c57ab8841120a77e08ac
patpbe <pat.pbe@instanweb.fr>
2017-01-26 11:46:50 +0100

    students list and show one student

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  
-  resources :courses
 
-#  get 'courses/edit'
+  get 'courses/edit'
 
-#  get 'courses/index'
+  get 'courses/index'
 
   get 'students/index'
 
-  get 'students/show'
+  get 'students/show/:id', :to => "students#show"
+
+#  get 'students/show/:id' => 'students#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
