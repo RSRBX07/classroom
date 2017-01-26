d3afb7b90d71f1eb7bc83dcbe5af378cb41cc0af
patpbe <pat.pbe@instanweb.fr>
2017-01-26 22:35:08 +0100

    remove course from students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126181416) do
+ActiveRecord::Schema.define(version: 20170126212419) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "field"
@@ -21,9 +21,8 @@ ActiveRecord::Schema.define(version: 20170126181416) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.string  "name"
-    t.integer "course_id"
-    t.string  "src_img"
+    t.string "name"
+    t.string "src_img"
   end
 
 end
