9a8a0afc4bf6f68cd654b6a742c95e7c3c578744
Pierre Merlin <Pierre Merlin>
2017-01-26 13:41:38 +0100

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'students/index'
 
-  get 'students/show'
+  get 'students/show/:id', :to => "students#show"
 
+  root "students#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
