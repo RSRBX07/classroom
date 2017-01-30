fa91970bba322d63fd31c66296bfe991e0a27701
Dodo <donovan.persent@gmail.com>
2017-01-30 15:12:50 +0100

    Test solve db error

--- db/migrate/20170126084434_add_default.rb
+++ db/migrate/20170126084434_add_default.rb
@@ -1,8 +1,8 @@
 class AddDefault < ActiveRecord::Migration[5.0]
   def change
     change_table :students do |table|
-      table.remove :picture, :string
-      table.column :picture, :string, :default => "http://istc-pc-test-media.cs.washington.edu/images/default-profile-pic.png"
+      table.remove :picture
     end
+    add_column :students, :picture, :string, :default => "http://istc-pc-test-media.cs.washington.edu/images/default-profile-pic.png"
   end
 end
