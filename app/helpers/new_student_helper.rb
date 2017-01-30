e9f8dc9361eeb832e8e97ac93939af29512e18f8
Dodo <donovan.persent@gmail.com>
2017-01-30 16:14:57 +0100

    Before Scafold

---
+++ app/helpers/new_student_helper.rb
@@ -0,0 +1,2 @@
+module NewStudentHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'students/show/:id', :to => "students#show" , :as => "student"
   get 'courses/show'
 
+  #post 'new_student/', to: 'new_student#new', as: 'new_student'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
